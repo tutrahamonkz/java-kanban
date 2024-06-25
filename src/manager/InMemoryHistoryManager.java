1b037f9ade56233c17d86ec8b217fbd6d9392c58
tutrahamon <tutrachamon@mail.ru>
2024-06-25 12:55:48 +0300

    Add InMemoryHistoryManager.java

---
+++ src/manager/InMemoryHistoryManager.java
@@ -0,0 +1,30 @@
+package manager;
+
+import tasks.Task;
+
+import java.util.ArrayList;
+
+public class InMemoryHistoryManager implements HistoryManager {
+
+    private static final int COUNT_HISTORY = 10;
+    private ArrayList<Task> taskHistory;
+
+    public InMemoryHistoryManager() {
+        taskHistory = new ArrayList<>();
+    }
+
+    @Override
+    public void add(Task task) {
+        if (taskHistory.size() < COUNT_HISTORY) {
+            taskHistory.add(task);
+        } else {
+            taskHistory.removeFirst();
+            taskHistory.add(task);
+        }
+    }
+
+    @Override
+    public ArrayList<Task> getHistory() {
+        return taskHistory;
+    }
+}
