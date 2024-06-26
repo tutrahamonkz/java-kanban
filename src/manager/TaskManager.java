33e4f121c6e7a8d766aaaf6889cf42a7a6f12d0f
tutrahamon <tutrachamon@mail.ru>
2024-06-26 09:34:06 +0300

    Convert subtask storage to subtask id by Epic

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -43,7 +43,7 @@ public interface TaskManager {
 
     void removeSubtask(int id);
 
-    ArrayList<Subtask> getSubtaskByEpic(Epic epic);
+    ArrayList<Integer> getSubtaskByEpic(Epic epic);
 
     ArrayList<Task> getHistory();
 }
