53bc38efb3a80649a9620dc6cba7a501890f9de5
tutrahamon <tutrachamon@mail.ru>
2024-06-26 17:10:46 +0300

    Add getIndexSubtaskId(Integer subtaskId)

--- src/tasks/Epic.java
+++ src/tasks/Epic.java
@@ -5,17 +5,41 @@ import java.util.ArrayList;
 public class Epic extends Task {
     private ArrayList<Integer> subtasksId;
 
-    public Epic(String title, ArrayList<String> descriptions, Status status, ArrayList<Integer> subtasksId) {
+    public Epic(String title, ArrayList<String> descriptions, Status status, Integer subtaskId) {
         super(title, descriptions, status);
-        this.subtasksId = subtasksId;
+        subtasksId = new ArrayList<>();
+        subtasksId.add(subtaskId);
+    }
+
+    public Epic(String title, ArrayList<String> descriptions, Status status) {
+        super(title, descriptions, status);
+        subtasksId = new ArrayList<>();
     }
 
     public ArrayList<Integer> getSubtasksId() {
         return subtasksId;
     }
 
-    public void setSubtasks(ArrayList<Integer> subtasksId) {
-        this.subtasksId = subtasksId;
+    public boolean setSubtask(Integer subtaskId) {
+        if (this.getId() != subtaskId) {
+            subtasksId.add(subtaskId);
+            return true;
+        }
+        return false;
+    }
+
+    public Integer getIndexSubtaskId(Integer subtaskId) {
+        int index = -1;
+        for (int i = 0; i < subtasksId.size(); i++) {
+            if (subtaskId.equals(subtasksId.get(i))) {
+                index = i;
+                break;
+            }
+        }
+        if (index >= 0) {
+            return index;
+        }
+        return null;
     }
 
     @Override
