6989d2853e7d1fdc4f1029cb124f74416d9dd770
tutrahamon <tutrachamon@mail.ru>
2024-06-26 09:33:09 +0300

    Convert subtask storage to subtask id

--- src/tasks/Epic.java
+++ src/tasks/Epic.java
@@ -3,24 +3,24 @@ package tasks;
 import java.util.ArrayList;
 
 public class Epic extends Task {
-    private ArrayList<Subtask> subtasks;
+    private ArrayList<Integer> subtasksId;
 
-    public Epic(String title, ArrayList<String> descriptions, Status status, ArrayList<Subtask> subtasks) {
+    public Epic(String title, ArrayList<String> descriptions, Status status, ArrayList<Integer> subtasksId) {
         super(title, descriptions, status);
-        this.subtasks = subtasks;
+        this.subtasksId = subtasksId;
     }
 
-    public ArrayList<Subtask> getSubtasks() {
-        return subtasks;
+    public ArrayList<Integer> getSubtasksId() {
+        return subtasksId;
     }
 
-    public void setSubtasks(ArrayList<Subtask> subtasks) {
-        this.subtasks = subtasks;
+    public void setSubtasks(ArrayList<Integer> subtasksId) {
+        this.subtasksId = subtasksId;
     }
 
     @Override
     public String toString() {
         String template = "Epic{title='%s', id=%d, status=%s, subtasks=%d}";
-        return String.format(template, getTitle(), getId(), getStatus(), getSubtasks().size());
+        return String.format(template, getTitle(), getId(), getStatus(), getSubtasksId().size());
     }
 }
