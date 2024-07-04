db0c75a46e437ba734786cecb3519237989a50fd
tutrahamon <tutrachamon@mail.ru>
2024-07-04 13:01:24 +0300

    Refactor getDefault()

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -2,7 +2,7 @@ package service;
 
 public class Managers {
 
-    public static TaskManager getDefault(HistoryManager historyManager) {
+    public static TaskManager getDefault() {
         return new InMemoryTaskManager();
     }
 
